64059075187c52a2f8638404a208716b90784992
Jon Eastin <picardncce@gmail.com>
2022-05-18 08:04:56 -0700

    completed significant portions of authorsjs and booksjs

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,27 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  return authors.find(author => author.id === id)
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  return books.find(book => book.id === id)
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  const checkedOut = new Array(),
+    checkedIn = new Array(),
+    allBooks = new Array()
 
-function getBorrowersForBook(book, accounts) {}
+    for (let i = 0; i < books.length; i++) {
+      if (books[i].borrows.every(ele => ele.returned === true)) checkedIn.push(books[i])
+      else checkedOut.push(books[i])
+    }
+    allBooks.push(checkedOut, checkedIn)
+    return allBooks
+}
+
+function getBorrowersForBook(book, accounts) {
+
+}
 
 module.exports = {
   findAuthorById,
